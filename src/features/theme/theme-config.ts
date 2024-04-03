415d660e4516fc23055c2e0350850e2670a98c4f
Luis Delgado (Option 4.0 AG) <luisdel@option40.com>
2024-04-03 17:19:04 +0000

    Changes the name of the app

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "Option 4.0 AG Chat";
+export const AI_DESCRIPTION = "Your personal corporate assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
